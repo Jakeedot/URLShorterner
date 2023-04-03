f1528410cfd332ed24a9b59aef24a153fc0ea21c
JakeeDot <86273620+Jakeedot@users.noreply.github.com>
2023-04-03 15:10:01 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,4 +1,4 @@
-# Don't Edit
+# Edit line 23 "ez4short.com" with your shortener website link
 
 from motor.motor_asyncio import *
 from config import *
@@ -20,7 +20,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "shortnerfly.com",
+            "base_site": "ez4short.com",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
