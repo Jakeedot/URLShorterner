998a6b153b8ce6fc96a5b9e27facfe8707396f84
JakeeDot <86273620+Jakeedot@users.noreply.github.com>
2023-04-03 18:31:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,28 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
+
+New chat
+
+
+jakeerhussein1997@gmail.com
+Code: import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Mandatory variables for the bot to start
 API_ID = int(os.getenv("API_ID", "Your Api Id"))
 API_HASH = os.environ.get("API_HASH", "Your Api Hash")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "").split(" ") if i.strip()] 
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
+DATABASE_URL = os.getenv("DATABASE_URL", "Mongodb url") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(ADMIN)
+ADMINS.append(Id Owned Id)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
